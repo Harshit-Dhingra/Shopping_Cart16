e1c2df73101770df9741dde3e3b451835c850bf0
Harshit Dhingra <harshitdhingra08@gmail.com>
2026-04-25 14:27:20 +0530

    commit

---
+++ src/main/java/com/ecom16/Shopping_Cart16/ShoppingCart16Application.java
@@ -0,0 +1,13 @@
+package com.ecom16.Shopping_Cart16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCart16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCart16Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecom16/Shopping_Cart16/ShoppingCart16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom16.Shopping_Cart16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCart16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
