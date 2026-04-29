250ddba2b6b4198513342f059968ee913b7b8950
Harshit Dhingra <harshitdhingra08@gmail.com>
2026-04-29 15:37:40 +0530

    Commit

--- src/main/java/com/ecom16/Shopping_Cart16/repository/CategoryRepository.java
+++ src/main/java/com/ecom16/Shopping_Cart16/repository/CategoryRepository.java
@@ -1,4 +1,6 @@
 package com.ecom16.Shopping_Cart16.repository;
 
-public interface CategoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
 }
---
+++ src/main/java/com/ecom16/Shopping_Cart16/service/CategoryService.java
@@ -0,0 +1,4 @@
+package com.ecom16.Shopping_Cart16.service;
+
+public interface CategoryService {
+}
